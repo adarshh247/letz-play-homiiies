e6da6622b586969461be0a7afe744645397521eb
adarshh247 <adarsh9394@gmail.com>
2026-03-21 00:26:03 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js';
-
+/*
 const envUrl = import.meta.env.VITE_SUPABASE_URL;
 const envKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
@@ -11,5 +11,7 @@ const supabaseUrl = envUrl && envUrl.startsWith('http')
 const supabaseAnonKey = envKey && envKey.length > 20
   ? envKey 
   : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloYnpoZnRuc3B6dmVwYmFhcXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNjA2NjYsImV4cCI6MjA4MjczNjY2Nn0.1Qb024p975GsaQpZjyBgMEi6YjyMyRU7NBiu6RGfRQc';
-
+*/
+const supabaseUrl = 'https://yhbzhftnspzvepbaaqwu.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloYnpoZnRuc3B6dmVwYmFhcXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNjA2NjYsImV4cCI6MjA4MjczNjY2Nn0.1Qb024p975GsaQpZjyBgMEi6YjyMyRU7NBiu6RGfRQc';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
